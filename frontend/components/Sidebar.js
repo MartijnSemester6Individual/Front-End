af0a75442992b03f484700a060b0885cde3f079d
MartijnBassa <martijnbassa2009@hotmail.com>
2022-05-09 13:41:41 +0200

    Update Sidebar.js

--- frontend/components/Sidebar.js
+++ frontend/components/Sidebar.js
@@ -20,7 +20,7 @@ function Sidebar() {
   const { data: session } = useSession();
   return (
     <div className="fixed hidden h-full flex-col items-center p-2 sm:flex xl:w-[340px] xl:items-start">
-      <div className="hoverAnimation flex h-14 w-14 items-center justify-center p-0 xl:ml-24">
+      <div className="flex items-center justify-center p-0 hoverAnimation h-14 w-14 xl:ml-24">
         <Image src="https://rb.gy/ogau5a" alt="" width={30} height={30} />
       </div>
       <div className="mt-2.5 space-y-2.5 xl:ml-24">
@@ -34,13 +34,12 @@ function Sidebar() {
         <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
       </div>
       <button
-        className="ml-auto mt-7 hidden h-12 w-56 rounded-full bg-twitter-blue text-center text-lg font-bold 
-            text-twitter-white shadow-md hover:bg-twitter-blue-hover xl:inline"
+        className="hidden w-56 h-12 ml-auto text-lg font-bold text-center rounded-full shadow-md mt-7 bg-twitter-blue text-twitter-white hover:bg-twitter-blue-hover xl:inline"
       >
         Tweet
       </button>
       <div
-        className="hoverAnimation mt-auto flex items-center justify-center text-twitter-white xl:ml-auto xl:-mr-5"
+        className="flex items-center justify-center mt-auto hoverAnimation text-twitter-white xl:ml-auto xl:-mr-5"
         onClick={signOut}
       >
         <img src={session.user.image} alt="" className="h-10 w-10 rounded-full xl:mr-2.5" />
@@ -48,7 +47,7 @@ function Sidebar() {
           <h4 className="font-bold">{session.user.username}</h4>
           <p className="text-[#6e767d]">@{session.user.tag}</p>
         </div>
-        <DotsHorizontalIcon className="ml-10 hidden h-5 xl:inline" />
+        <DotsHorizontalIcon className="hidden h-5 ml-10 xl:inline" />
       </div>
     </div>
   );
